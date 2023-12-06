7a92fb03e124a8493beca01e7c9326f4174a09b6
batskywalker <60446365+batskywalker@users.noreply.github.com>
2023-12-06 01:01:02 -0800

    I'm so upset

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-const dotenv = require('dotenv');
-const { Client, GatewayIntentBits, Partials, ActionRowBuilder, ButtonBuilder, ButtonStyle, MessageComponentInteraction, InteractionCollector, ButtonComponent, InteractionResponse, Events, Collection } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildVoiceStates], partials: [Partials.Channel] });
-
 window.onload = function() {
     console.log("please work")
-    client.on("ready", () => {
-        console.log(`Logged in as ${client.user.tag}!`);
-    });
 
     let paragraph = window.document.querySelector("#test");
 
     paragraph.textContent = "this worked";
 }
-
-client.login(process.env.DISCORD);
