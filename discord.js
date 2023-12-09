d6504e0e439fb6849a5bb26b5fcb0b30397b174f
batskywalker <60446365+batskywalker@users.noreply.github.com>
2023-12-08 23:19:27 -0800

    I'm not sure this is working

--- discord.js
+++ discord.js
@@ -5,6 +5,7 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBit
 const express = require('express');
 const app = express();
 const hbs = require('hbs');
+const fs = require('fs');
 
 /*app.set('view engine', 'html');
 app.engine('html', require('hbs').__express);*/
@@ -25,16 +26,26 @@ app.get("/", function (req, res) {
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    
-})
+    fs.readFile('website/index.html', function(err, data) {
+        res.writeHead(200, {
+            'Content-Type': 'text/html',
+            'Content-Length': data.length
+        });
+        res.write(data);
+        res.end();
+    })
+}).listen(3000);
+
+/*server.listen(3000, () => {
+    console.log('Server listening on port 3000');
+});*/
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`);
-})
-
-client.on("messageCreate", (msg) => {
-    console.log(msg.content);
-    
 });
+
+/*client.on("messageCreate", (msg) => {
+    
+});*/
 
 client.login(process.env.DISCORD);
--- website/index.js
+++ website/index.js
@@ -1,3 +1,9 @@
-window.onload = function() {
+/*window.onload = function() {
     document.getElementById("test").textContent = "This did work";
+}*/
+
+const eventSource = new EventSource('https://localhost:3000');
+
+eventSource.onmessage = event => {
+    console.log(`received message from server: ${event.data}`);
 }
