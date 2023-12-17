eaa00067ccd91231c63155eadc02dcc7663e23e9
batskywalker <60446365+batskywalker@users.noreply.github.com>
2023-12-17 00:42:50 -0800

    somethings gotta work

--- commands/move.js
+++ commands/move.js
@@ -1,24 +1,23 @@
 const {SlashCommandBuilder} = require('discord.js');
 
-module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('move')
-        .setDescription('Use one action point to move one space up, down, left, or right')
-        .addStringOption(option =>
-            option.setName('direction')
-                .setDescription('The direction you want to move')
-                .setRequired(true)
-                .addChoices(
-                    {name: 'up', value: 'up'},
-                    {name: 'down', value: 'down'},
-                    {name: 'left', value: 'left'},
-                    {name: 'right', value: 'right'}
-                )),
-    async execute(interaction) {
-        const moveData = {
-            player: interaction.user.id,
-            move: interaction.options.getString('direction')
-        }
-        return moveData
-    },
+module.exports.data = new SlashCommandBuilder()
+.setName('move')
+.setDescription('Use one action point to move one space up, down, left, or right')
+.addStringOption(option =>
+option.setName('direction')
+.setDescription('The direction you want to move')
+.setRequired(true)
+.addChoices(
+    {name: 'up', value: 'up'},
+    {name: 'down', value: 'down'},
+    {name: 'left', value: 'left'},
+    {name: 'right', value: 'right'}
+));
+
+module.exports.execute = async (interaction) => {
+    const moveData = {
+        player: interaction.user.id,
+        move: interaction.options.getString('direction')
+    }
+    return moveData
 };
