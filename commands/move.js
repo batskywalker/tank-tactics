24b67dffd1c4f844adb0010bf56bb1b1d8e50cbd
batskywalker <60446365+batskywalker@users.noreply.github.com>
2023-12-21 04:01:14 -0800

    move command being worked on

--- commands/move.js
+++ commands/move.js
@@ -9,18 +9,26 @@ option.setName('direction')
 .setDescription('The direction you want to move')
 .setRequired(true)
 .addChoices(
-    {name: 'up', value: 'up'},
-    {name: 'down', value: 'down'},
-    {name: 'left', value: 'left'},
-    {name: 'right', value: 'right'}
+    {name: 'up', value: [0, -1]},
+    {name: 'down', value: [0, 1]},
+    {name: 'left', value: [-1, 0]},
+    {name: 'right', value: [1, 0]}
 ));
 
 async function execute(interaction, playerData) {
-    const moveData = {
-        player: interaction.user.id,
-        move: interaction.options.getString('direction')
+    const direction = interaction.data.options[0].value;
+    var currentPlayer;
+    var num;
+    for (var i = 0; i < playerData.length; i++) {
+        if (interaction.user.id == playerData[i].playerID) {
+            currentPlayer = playerData[i];
+            num = i;
+        }
     }
-    return moveData
+
+    if ((currentPlayer.pos.x + direction[0] >= 0 && currentPlayer.pos.x + direction[0] < 30) && (currentPlayer.pos.y + direction[1] >= 0 && currentPlayer.pos.y + direction[1] < 30) && currentPlayer.action > 0) {
+        currentPlayer.action
+    }
 }
 
 export default {data, execute};
